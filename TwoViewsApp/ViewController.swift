fa8c4671412330e59aa04eaa27e398d1f7b4ff29
Yefim Yakinin <efim.yakunin@gmail.com>
2020-08-19 13:08:29 +0300

    Initial Commit

---
+++ TwoViewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoViewsApp
+//
+//  Created by Yefim Yakunin on 19.08.2020.
+//  Copyright © 2020 Yefim Yakunin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
