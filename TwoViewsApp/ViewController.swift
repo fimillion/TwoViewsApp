b427341216ea38a56ba7c6c22d7e4f04f4f8830c
Yefim Yakinin <efim.yakunin@gmail.com>
2020-08-20 15:11:32 +0300

    Update constraint

--- TwoViewsApp/ViewController.swift
+++ TwoViewsApp/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  TwoViewsApp
-//
-//  Created by Yefim Yakunin on 19.08.2020.
-//  Copyright © 2020 Yefim Yakunin. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
